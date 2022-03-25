3f996b81efba194320f0601913a718b1bd56b499
IsmaDevBalmis <75784444+IsmaDevBalmis@users.noreply.github.com>
2022-03-25 01:01:03 +0100

    Creo el RootController con la navigation de slides

---
+++ YoutubeClone/YoutubeClone/Main/MainViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MainViewController.swift
+//  YoutubeClone
+//
+//  Created by Ismael Ruiz on 25/3/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    var rootPageViewController : RootPageViewController!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? RootPageViewController{
+            destination.delegateRoot = self
+            rootPageViewController = destination
+        }
+    }
+    
+    
+  
+
+}
+
+
+extension MainViewController : RootPageProtocol{
+    func currentPage(_ index: Int) {
+        print("currentPage: ", index)
+    }
+    
+    
+}
